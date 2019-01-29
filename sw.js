a9c4e81b2c43b0788111a7f00acf095f3e21d8bd
Evgenik <evkam@mail.ru>
2019-01-30 01:19:49 +0300

    pwa

--- sw.js
+++ sw.js
@@ -2,7 +2,30 @@ self.addEventListener('install', e => {
     e.waitUntil(
         caches.open('easyDarts-cache').then(cache => {
             return cache.addAll([
-
+              root + '',
+              root + 'index.html',
+              root + 'android-chrome-192x192.png',
+              root + 'android-chrome-512x512.png',
+              root + 'apple-touch-icon.png',
+              root + 'browserconfig.xml',
+              root + 'endings.js',
+              root + 'favicon-16x16.png',
+              root + 'favicon-32x32.png',
+              root + 'favicon.ico',
+              root + 'icon.png',
+              root + 'indexedDB.js',
+              root + 'language.js',
+              root + 'media.css',
+              root + 'menu.css',
+              root + 'mstile-150x150.png',
+              root + 'safari-pinned-tab.svg',
+              root + 'settings.js',
+              root + 'site.manifest',
+              root + 'styles.css',
+              root + 'sw.js',
+              root + 'swipe.js',
+              root + 'vue.js',
+              root + 'vueCommands.js'
             ]);
         })
     );
