6d54d56fc78747c7fad7583a6d318fa0102456c4
Evgenik <evkam@mail.ru>
2019-01-30 01:21:52 +0300

    pwa

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open('easyDarts-cache').then(cache => {
+            var root = "/EasyDarts/";
             return cache.addAll([
               root + '',
               root + 'index.html',
