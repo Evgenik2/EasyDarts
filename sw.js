6fd15ab459ec864e30b286ec47313c6440f8b21b
Evgenik <evkam@mail.ru>
2019-01-31 07:49:32 +0300

    pwa

--- sw.js
+++ sw.js
@@ -2,32 +2,32 @@ self.addEventListener('install', e => {
     self.skipWaiting(); 
     e.waitUntil(
         caches.open('easyDarts-cache').then(cache => {
-            var root = "/EasyDarts/";
+            //var root = "/EasyDarts/";
             return cache.addAll([
-              root + '',
-              root + 'index.html',
-              root + 'android-chrome-192x192.png',
-              root + 'android-chrome-512x512.png',
-              root + 'apple-touch-icon.png',
-              root + 'browserconfig.xml',
-              root + 'endings.js',
-              root + 'favicon-16x16.png',
-              root + 'favicon-32x32.png',
-              root + 'favicon.ico',
-              root + 'icon.png',
-              root + 'indexedDB.js',
-              root + 'language.js',
-              root + 'media.css',
-              root + 'menu.css',
-              root + 'mstile-150x150.png',
-              root + 'safari-pinned-tab.svg',
-              root + 'settings.js',
-              root + 'site.webmanifest',
-              root + 'styles.css',
-              root + 'sw.js',
-              root + 'swipe.js',
-              root + 'vue.js',
-              root + 'vueComponents.js'
+              '/',
+              'index.html',
+              'android-chrome-192x192.png',
+              'android-chrome-512x512.png',
+              'apple-touch-icon.png',
+              'browserconfig.xml',
+              'endings.js',
+              'favicon-16x16.png',
+              'favicon-32x32.png',
+              'favicon.ico',
+              'icon.png',
+              'indexedDB.js',
+              'language.js',
+              'media.css',
+              'menu.css',
+              'mstile-150x150.png',
+              'safari-pinned-tab.svg',
+              'settings.js',
+              'site.webmanifest',
+              'styles.css',
+              'sw.js',
+              'swipe.js',
+              'vue.js',
+              'vueComponents.js'
             ]);
         })
     );
